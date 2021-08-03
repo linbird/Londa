ee27ffba3cd73b1435b8a236ae6b4ea7a12493b0
linbird <zhangqw1213@gmail.com>
2021-08-03 20:00:17 +0800

    SystemV共享内存

--- code/IPC/SHM/SystemV/write_node.cpp
+++ code/IPC/SHM/SystemV/write_node.cpp
@@ -1,6 +1,9 @@
+#include <exception>
+#include <fstream>
 #include <iostream>
 #include <string>
 #include <sstream>
+#include <sys/ipc.h>
 #include <sys/shm.h>
 #include <semaphore.h>
 #include <sys/types.h>
@@ -13,43 +16,57 @@
 
 #include <mutex>
 
-class SHM_manager{
-    private;
-    ket_t 
-};
-
 using namespace std;
 using namespace chrono_literals;
 
-int writer(chrono::seconds& limit, void* address){
-    auto start_point = chrono::steady_clock::now();
-    auto draft = start_point - start_point;
-    int index = 0;
-    stringstream data_ssr;
-    data_ssr << "进程(" << getpid() << ")线程(" << this_thread::get_id() << "写入数据：" ;
-    do{
-        stringstream data(data_ssr);
-        data << index;
-        cout << data.str() << endl;
-        strncpy(address, data.str().data(), data.str().size());
-        ++index;
-        draft = chrono::duration_cast<chrono::milliseconds>(chrono::steady_clock::now() - start_point);
-    }while(draft <= limit);
-    return index;
-}
+struct SHM{
+    mutex* protecter;
+    char data[1024];
+    SHM(){
+        cout << __FUNCTION__ << endl;
+//        protecter.lock();
+//        protecter.unlock();
+        memset(data, 0, 1024);
+    }
+};
+
+//int writer(string path, void* address){
+//    fstream file(path);
+//    auto origin_buf = cin.rdbuf();
+//    cin.rdbuf(file.rdbuf());
+//    for(string line; getline(cin, line); line.clear()){
+//        cout << line << endl;
+//        SHM *shm = (SHM*)address;
+//        unique_lock<mutex> locker(shm->protecter);
+////        copy(line.begin(), line.end(), (char*)(address) + sizeof(mutex));
+//        this_thread::sleep_for(10ms);
+//    }
+//    cin.rdbuf(origin_buf);
+//    return 0;
+//}
 
 int main(int argc, char* argv[]){
     const string path(argv[1]);
     key_t id = ftok(path.c_str(), 0);
-    int erron = shmget(id, 128*1024, 0644);
+    cout << id << endl;
+    int erron = shmget(id, sizeof(SHM), 0644|IPC_CREAT);
     if(erron == -1){
         cerr << "创建共享内存失败，检查文件"<< path << "是否存在" << endl;
         return -1;
     }
-    void* address = shmat(id, nullptr, 0);
-    packaged_task<int(chrono::seconds, void*)> task(writer);
-    future<int> count = task.get_future();
-    thread(task, 2s, address).detach();
-    cout << "写进程写入" << count.get() << "个数据" << endl;
+    SHM* address = (SHM*)shmat(id, nullptr, 0);
+    address->protecter = new(address) mutex();
+    try{
+//addres = new(address) SHM;
+    }catch(exception& e){
+        cout << e.what() << endl;
+        return 0;
+    }
+//    packaged_task<int(string, void*)> task(writer);
+//    future<int> count = task.get_future();
+//    thread(move(task), path, addres).join();
+//    cout << "写进程写入" << count.get() << "个数据" << endl;
+    shmdt(address);
+    shmctl(id, IPC_RMID, 0);
     return 0;
 }
